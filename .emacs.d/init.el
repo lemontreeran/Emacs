25fe4fccb0067d9e589e25bf2f337ef8b5a08c77
Lemontreeran <lemontreeran@Lemontreeran.local>
2018-03-19 10:02:29 +1100

    Seperate custom packages from init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,64 +1,16 @@
-;;;; PACKAGE REPOSITORIES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ;("marmalade" . "http://marmalade-repo.org/packages/")
-                          ("melpa" . "http://melpa.milkbox.net/packages/")))
-)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-;(setq package-enable-at-startup nil)
 
-(require 'cl)
- ;; Add Packages
- (defvar my/packages '(
-                ;; --- Auto-completion ---
-                company
-                ;; --- Better Editor ---
-                hungry-delete
-                swiper
-                counsel
-                smartparens
-                ;; --- Major Mode ---
-                js2-mode
-                ;; --- Minor Mode ---
-                nodejs-repl
-                exec-path-from-shell
-                ;; --- Themes ---
-                monokai-theme
-                ;; solarized-theme
-                ) "Default packages")
- (setq package-selected-packages my/packages)
- (defun my/packages-installed-p ()
-     (loop for pkg in my/packages
-           when (not (package-installed-p pkg)) do (return nil)
-           finally (return t)))
- (unless (my/packages-installed-p)
-     (message "%s" "Refreshing package database...")
-     (package-refresh-contents)
-     (dolist (pkg my/packages)
-       (when (not (package-installed-p pkg))
-         (package-install pkg))))
- ;; Find Executable Path on OS X
- (when (memq window-system '(mac ns))
-   (exec-path-from-shell-initialize))
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
-;Install theme
-(add-to-list 'my/packages 'monokai-theme)
-(load-theme 'monokai 1)
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
-;show match parents
-(require 'smartparens-config)
-;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(smartparens-global-mode t)
-(show-smartparens-global-mode t)
+(require 'init-packages)
 
 ;Auto revert file when they are changed outside the editor
 (global-auto-revert-mode t)
-
-;Delete multiple spaces 
-(require 'hungry-delete)
-(global-hungry-delete-mode)
 
 ;Setup the F2 key to open init.el file
 (defun open-my-init-file()
@@ -67,11 +19,6 @@
 (global-set-key (kbd "<f2>") 'open-my-init-file)
 ;Enable row number
 (global-linum-mode t)
-;Open company mode
-(global-company-mode t)
-;Setup the C-x C-r key to open recent files
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
 (global-set-key "\C-s" 'swiper)
 ;(global-set-key (kbd "C-x C-r") 'recentf-open-files)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
@@ -94,7 +41,6 @@
 (require 'recentf)
 (recentf-mode 1)
 (setq recentf-max-menu-item 10)
-
 ;Cancel welcome page
 (setq inhibit-splash-screen t)
 ;Set the crusor same as the other editor
@@ -104,15 +50,20 @@
 ;Disable backup files
 (setq make-backup-files nil)
 ;;Disalbe auto save
-(setq auto-save-default nill)
+(setq auto-save-default nil)
 ;;Open with full screen
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 (setq cfs-keep-frame-size nil)
-;; config js2-mode for js files
-(setq auto-mode-alist
-	  (append
-	   '(("\\.js\\'" . js2-mode))
-	   auto-mode-alist))
+;;disable audio bell
+(setq ring-bell-function 'ignore)
+
+(setq-default abbrev-mode t)
+
+(define-abbrev-table 'global-abbrev-table '(
+                                            ;; signature
+                                            ("8lem" "lemontreeran")
+                                            ;; emacs regex
+                                            ))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
---
+++ .emacs.d/lisp/init-packages.el
@@ -0,0 +1,79 @@
+;;;; PACKAGE REPOSITORIES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'cl)
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ;("marmalade" . "http://marmalade-repo.org/packages/")
+                          ("melpa" . "http://melpa.milkbox.net/packages/")))
+)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+;(setq package-enable-at-startup nil)
+
+
+ ;; Add Packages
+ (defvar my/packages '(
+                ;; --- Auto-completion ---
+                company
+                ;; --- Better Editor ---
+                hungry-delete
+                swiper
+                counsel
+                smartparens
+		popwin
+                ;; --- Major Mode ---
+                js2-mode
+                ;; --- Minor Mode ---
+                nodejs-repl
+                exec-path-from-shell
+                ;; --- Themes ---
+                monokai-theme
+                ;; solarized-theme
+                ) "Default packages")
+ (setq package-selected-packages my/packages)
+ (defun my/packages-installed-p ()
+     (loop for pkg in my/packages
+           when (not (package-installed-p pkg)) do (return nil)
+           finally (return t)))
+ (unless (my/packages-installed-p)
+     (message "%s" "Refreshing package database...")
+     (package-refresh-contents)
+     (dolist (pkg my/packages)
+       (when (not (package-installed-p pkg))
+         (package-install pkg))))
+
+ ;; Find Executable Path on OS X
+ (when (memq window-system '(mac ns))
+   (exec-path-from-shell-initialize))
+
+;Install theme
+(add-to-list 'my/packages 'monokai-theme)
+(load-theme 'monokai 1)
+
+;;Popup window Management
+(require 'popwin)
+(popwin-mode 1)
+
+;show match parents
+(require 'smartparens-config)
+;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(smartparens-global-mode t)
+(show-smartparens-global-mode t)
+
+;Delete multiple spaces 
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+;Open company mode
+(global-company-mode t)
+
+;Setup the C-x C-r key to open recent files
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+;; config js2-mode for js files
+(setq auto-mode-alist
+	  (append
+	   '(("\\.js\\'" . js2-mode))
+	   auto-mode-alist))
+
+(provide 'init-packages)
