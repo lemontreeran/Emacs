2b29233dc6f786864f8b8b5926b76191b48ccb9a
Lemontreeran <lemontreeran@Lemontreeran.local>
2018-07-18 21:58:54 +1000

    Spacemacs private setup

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,28 +1,34 @@
+;;; init.el --- Spacemacs Initialization File
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+;; Without this comment emacs25 adds (package-initialize) here
+;; (package-initialize)
 
-(require 'cask "~/.cask/cask.el")
-(cask-initialize) 
-(require 'pallet)
-(pallet-mode t)
+;; Increase gc-cons-threshold, depending on your system you may set it back to a
+;; lower value in your dotfile (function `dotspacemacs/user-config')
+(setq gc-cons-threshold 100000000)
 
-(add-to-list 'load-path "~/.emacs.d/lisp")
+(defconst spacemacs-version         "0.200.13" "Spacemacs version.")
+(defconst spacemacs-emacs-min-version   "24.4" "Minimal version of Emacs.")
 
-;Setup the F2 key to open init.el file
-(defun open-my-init-file()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
-
-(require 'init-packages)
-(require 'init-ui)
-(require 'init-better-defaults)
-(require 'init-keybindings)
-(require 'init-org)
-
-;;Setup customize-group config
-(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
-(load custom-file 'no-error 'no-message)
+(if (not (version<= spacemacs-emacs-min-version emacs-version))
+    (error (concat "Your version of Emacs (%s) is too old. "
+                   "Spacemacs requires Emacs version %s or above.")
+           emacs-version spacemacs-emacs-min-version)
+  (load-file (concat (file-name-directory load-file-name)
+                     "core/core-load-paths.el"))
+  (require 'core-spacemacs)
+  (spacemacs/init)
+  (configuration-layer/sync)
+  (spacemacs-buffer/display-startup-note)
+  (spacemacs/setup-startup-hook)
+  (require 'server)
+  (unless (server-running-p) (server-start)))
