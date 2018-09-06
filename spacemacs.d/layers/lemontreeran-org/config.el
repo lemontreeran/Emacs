d3b1f217f392bb384ee833a2c493b60d0367ed70
Ran <ran.xing@gmail.com>
2018-09-06 10:59:28 +1000

    install espell flycheck

---
+++ spacemacs.d/layers/lemontreeran-org/config.el
@@ -0,0 +1,7 @@
+(defun lemontreeran-org/org-ispell ()
+  "Configure `ispell-skip-region-alist' for `org-mode'."
+  (make-local-variable 'ispell-skip-region-alist)
+  (add-to-list 'ispell-skip-region-alist '(org-property-drawer-re))
+  (add-to-list 'ispell-skip-region-alist '("~" "~"))
+  (add-to-list 'ispell-skip-region-alist '("=" "="))
+  (add-to-list 'ispell-skip-region-alist '("^#\\+BEGIN_SRC" . "^#\\+END_SRC")))
